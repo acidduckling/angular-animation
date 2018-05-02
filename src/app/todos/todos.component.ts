5f6935a7fc2d9b04a1dbe1983d9cd39a282436b8
Clayton Wakefield <acidduckling@gmail.com>
2018-05-02 15:26:40 +1000

    200 - Animation Callbacks

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -49,4 +49,11 @@ export class TodosComponent {
     const index = this.items.indexOf(item);
     this.items.splice(index, 1);
   }
+
+  animationStarted($event) {
+    console.log($event);
+  }
+  animationDone($event) {
+    console.log($event);
+  }
 }
